fa2a0d10d302105d4c861224b9f15531b9866e97
smartpro1 <akeni.promise@yahoo.com>
2020-03-12 09:55:32 +0100

    first commit

---
+++ src/main/java/com/promise/MyProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectManagerApplication.class, args);
+	}
+
+}
