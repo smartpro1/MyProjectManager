eaf43e8322c520336072b17c555b4931a319042f
smartpro1 <akeni.promise@yahoo.com>
2020-03-31 13:32:10 +0100

    modify security constant

--- src/main/java/com/promise/security/SecurityConstants.java
+++ src/main/java/com/promise/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
 
-  public static final Integer EXPIRATION_TIME = 864_000_000;
+  public static final Integer EXPIRATION_TIME = 86_400_000;
 
 }
