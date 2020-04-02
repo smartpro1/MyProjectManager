ca794677e4832cd4a8e44e7f50955fb2a8200e30
smartpro1 <akeni.promise@yahoo.com>
2020-04-03 00:30:34 +0100

    sending reset link done

--- src/main/java/com/promise/MyProjectManagerApplication.java
+++ src/main/java/com/promise/MyProjectManagerApplication.java
@@ -21,22 +21,5 @@ public class MyProjectManagerApplication {
 		SpringApplication.run(MyProjectManagerApplication.class, args);
 	}
 	
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("my.gmail@gmail.com");
-	    mailSender.setPassword("password");
-	     
-	    java.util.Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	     
-	    return mailSender;
-	}
 }
 
--- src/main/java/com/promise/services/EmailService.java
+++ src/main/java/com/promise/services/EmailService.java
@@ -1,7 +1,22 @@
 package com.promise.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-public interface EmailService {
-	public void sendEmail(SimpleMailMessage email);
+@Service
+public class EmailService {
+    
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	public void sendEmail(SimpleMailMessage email) {
+//		SimpleMailMessage mail = new SimpleMailMessage();
+//		mail.setTo(email);
+//		mail.setFrom("support@myprojectManager.com");
+//		mail.setSubject("Password Reset");
+//		mail.setText(text);
+		javaMailSender.send(email);
+	}
 }
